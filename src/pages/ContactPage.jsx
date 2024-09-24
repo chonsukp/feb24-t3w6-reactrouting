ab3ffba9ddf96596451d588efbee6a6afc3f2aca
chonsukp <chonsukp@gmail.com>
2024-09-24 20:01:19 +1000

    feat: redirect button

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,12 @@
+import RedirectButton from "../components/RedirectButton";
 
 export default function ContactPage(){
 
     return(
         <div>
             <h1>Contact Me!</h1>
+
+            <RedirectButton/>
         </div>
     );
 }
